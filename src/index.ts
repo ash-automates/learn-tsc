26f32ee29322968a34c01cdc42339b301b37c292
Hachem <s.hachem@murena.io>
2024-06-04 17:55:25 +0000

    Added code for lesson 12

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,48 @@
-//--------------
-// type aliases
-//--------------
-
-// example 1 - tuple
-
-type Rgb = [number, number, number];
-
-function getRandomColor(): Rgb {
-  const r = Math.floor(Math.random() * 255);
-  const g = Math.floor(Math.random() * 255);
-  const b = Math.floor(Math.random() * 255);
-
-  return [r, g, b];
-}
-
-const colorOne = getRandomColor();
-const colorTwo = getRandomColor();
-console.log(colorOne, colorTwo);
-
-// example 2 - object literal
-
-type User = {
-  name: string;
-  score: number;
-};
-
-const userOne: User = { name: "mario", score: 75 };
-
-function formatUser(user: User): void {
-  console.log(`${user.name} has a score of ${user.score}.`);
-}
-
-formatUser(userOne);
-formatUser({ name: "yoshi", score: 100 });
 //-------------
-// union types
+// type guards
 //-------------
 
-let someId: number | string;
-
-someId = 1;
-someId = "2";
-
-let email: string | null = null;
-
-email = "mario@netninja.dev";
-email = null;
+// example 1
 
 type Id = number | string;
-let anotherId: Id;
-
-// anotherId = undefined
-anotherId = "1";
-anotherId = 2;
-
-//--------------------
-// union type pitfall
-//--------------------
 
 function swapIdType(id: Id): Id {
-  // can only use props and methods common to
-  // both number and string types
-  // parseInt(id) --> not allowed
+  if (typeof id === "string") {
+    // can use string methods and properties
+    return parseInt(id);
+  } else {
+    // can use number methods and properties
+    return id.toString();
+  }
+}
 
-  return id;
+const idOne = swapIdType(1);
+const idTwo = swapIdType("2");
+
+console.log(idOne, idTwo);
+
+//--------------------
+// tagged union types
+//--------------------
+
+interface User {
+  type: "user";
+  username: string;
+  email: string;
+  id: Id;
+}
+interface Person {
+  type: "person";
+  firstname: string;
+  age: number;
+  id: Id;
+}
+
+function logDetails(value: User | Person): void {
+  if (value.type === "user") {
+    console.log(value.email, value.username);
+  }
+  if (value.type === "person") {
+    console.log(value.firstname, value.age);
+  }
 }
